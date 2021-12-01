0bb86134cbd54f8b66c3a9690f108e7ee5e48c8b
Pablo Guimarães <pablomaiden@gmail.com>
2021-12-01 08:07:24 -0300

    Arquivos de excessão

---
+++ src/main/java/br/com/phc/brasileiraoapi/config/ModelMapperConfig.java
@@ -0,0 +1,10 @@
+package br.com.phc.brasileiraoapi.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+	
+	
+
+}
--- src/main/java/br/com/phc/brasileiraoapi/exception/ResourceExceptionHandler.java
+++ src/main/java/br/com/phc/brasileiraoapi/exception/ResourceExceptionHandler.java
@@ -2,10 +2,12 @@ package br.com.phc.brasileiraoapi.exception;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -20,9 +22,46 @@ public class ResourceExceptionHandler extends ResponseEntityExceptionHandler{
 	
 	@ExceptionHandler(UnathorizedException.class)
 	public ResponseEntity<StandartError> badRequestException(UnathorizedException e, HttpServletRequest request){		
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-			   .body(new StandartError(HttpStatus.BAD_REQUEST, e.getMessage(), request.getRequestURI()));
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+			   .body(new StandartError(HttpStatus.UNAUTHORIZED, e.getMessage(), request.getRequestURI()));
 		
 	}	
+	
+	@ExceptionHandler(ForbiddenException.class)
+	public ResponseEntity<StandartError> badRequestException(ForbiddenException e, HttpServletRequest request){		
+		return ResponseEntity.status(HttpStatus.FORBIDDEN)
+			   .body(new StandartError(HttpStatus.FORBIDDEN, e.getMessage(), request.getRequestURI()));
+		
+	}	
+	
+	@ExceptionHandler(InternalServerException.class)
+	public ResponseEntity<StandartError> badRequestException(InternalServerException e, HttpServletRequest request){		
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+			   .body(new StandartError(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), request.getRequestURI()));
+		
+	}	
+	
+	@ExceptionHandler(NotFoundException.class)
+	public ResponseEntity<StandartError> badRequestException(NotFoundException e, HttpServletRequest request){		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND)
+			   .body(new StandartError(HttpStatus.NOT_FOUND, e.getMessage(), request.getRequestURI()));
+		
+	}
+	
+	@ExceptionHandler(RuntimeException.class)
+	public ResponseEntity<StandartError> badRequestException(RuntimeException e, HttpServletRequest request){		
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+			   .body(new StandartError(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), request.getRequestURI()));
+		
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<Object> exception(Exception e, Object body, HttpStatus httpStatus, 
+			HttpHeaders httpHeaders, WebRequest webRequest){		
+		return ResponseEntity.status(httpStatus)
+				   .body(new StandartError(httpStatus, e.getMessage(), webRequest.getContextPath()));
+		
+	}
+	
 
 }
